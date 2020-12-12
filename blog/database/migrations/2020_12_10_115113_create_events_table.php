2bcf0885fd152d5655c0ccc91ed8a1c3826dab90
Leveofan1197 <Programma1197>
2020-12-12 18:18:39 +0500

    Done DB

--- blog/database/migrations/2020_12_10_115113_create_events_table.php
+++ blog/database/migrations/2020_12_10_115113_create_events_table.php
@@ -21,18 +21,17 @@ class CreateEventsTable extends Migration
             $table->text("full_description")->nullable();
             $table->string("short_description");
             $table->integer("max_people_count")->nullable();
-            $table->integer("access");
             $table->dateTime("start_at");
             $table->dateTime("finish_at");
             $table->foreignId('author_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->boolean("private");
-            $table->integer("age_from")->nullable();
-            $table->integer("age_to")->nullable();
+            $table->boolean("private")->default(0);
+            $table->char("age_from")->nullable();
+            $table->char("age_to")->nullable();
             $table->integer("price");
             $table->string("insta_link")->nullable();
             $table->string("site_link")->nullable();
             $table->string("vk_link")->nullable();
-            $table->float("rating");
+            $table->float("rating")->default(0);
             $table->boolean("active");
             
         });
