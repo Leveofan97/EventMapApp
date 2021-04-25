0700b4bb740c6dca20f89e9c5dba803d7f3dc0a8
Leveofan1197 <Programma1197>
2021-04-26 01:36:35 +0500

    + aboba

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,28 +18,32 @@ class EventController extends Controller
                 'address' => ['required', 'max:255'],
                 'coordinates' => 'required',
                 'short_description' => ['required', 'string', 'max:255'],
-                'start_at' => 'required',
-                'finish_at' => 'required',
+                'start_at'=>'required',
                 'author_id' => 'required',
-                'price' => 'required',
-                'active' => 'required',
+                'private'=> 'required',
             ]);
         }
 
+    protected function getCategory(){
+       return DB::table('categories')
+           ->get();
+    }
+
     protected function save(Request $data){
-        if(Auth::check()){
-            return Event::create([
-                'name' => $data['name'],
-                'address' => $data['address'],
-                'coordinates' => $data['coordinates'],
-                'short_description' => $data['short_description'],
-                'start_at' => $data['start_at'],
-                'finish_at' => $data['finish_at'],
-                'author_id' => $data['author_id'],
-                'price' => $data['price'],
-                'active' => '0',
+        return Event::create([
+            'name' => $data['name'],
+            'address' => $data['address'],
+            'coordinates' => $data['coordinates'],
+            'short_description' => $data['short_description'],
+            'full_description' => $data['short_description'],
+            'start_at' => $data['start_at'],
+            'finish_at' => $data['finish_at'],
+            'author_id' => $data['author_id'],
+            'private' => $data['private'],
+            'price' => $data['price'],
+            'age_from'=>$data['age_from'],
+            'age_to'=>$data['age_to'],
             ]);
-        }
     }
      // Метод вывода всех событий
      public function index()
--- routes/api_user.php
+++ routes/api_user.php
@@ -19,20 +19,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['prefix' => 'user'], function () {
-    //Route::post('logintest', [LoginController::class, 'login']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('registration',[RegisterController::class, 'create']);
-    Route::post('newevent',[EventController::class, 'save']);
     Route::get('listevents',[EventController::class, 'index']);
 
     Route::group([
         'middleware' => ['auth:web'],
     ], function () {
-        // Route::put('profile', [ProfileController::class, 'update']);
-
-        // Route::resources([
-        // 'orders' => OrderController::class,
-        // ]);
+        Route::post('newevent',[EventController::class, 'save']);
+        Route::get('getcategories',[EventController::class, 'getCategory']);
     });
 });
