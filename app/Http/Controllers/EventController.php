cfdc1d60e16798cfe583373e5a0f2a5a4dc49329
Leveofan1197 <Programma1197>
2021-04-26 00:25:02 +0500

    + Add protect method save events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,25 +6,41 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
     // Метод сохранения событий в базу
-    public function save (Request $request){
-        $validateFields = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'address' => ['required', 'max:255'],
-            'coordinates' => 'required',
-            'short_description' => ['required', 'string', 'max:255'],
-            'start_at' => 'required',
-            'finish_at' => 'required',
-            'author_id' => 'required',
-            'price' => 'required',
-            'active' => 'required',
-        ]);
-        $user = Event::create($validateFields);
-    }
+    protected function validator(array $data)
+        {
+            return Validator::make($data, [
+                'name' => ['required', 'string', 'max:255'],
+                'address' => ['required', 'max:255'],
+                'coordinates' => 'required',
+                'short_description' => ['required', 'string', 'max:255'],
+                'start_at' => 'required',
+                'finish_at' => 'required',
+                'author_id' => 'required',
+                'price' => 'required',
+                'active' => 'required',
+            ]);
+        }
 
+    protected function save(Request $data){
+        if(Auth::check()){
+            return Event::create([
+                'name' => $data['name'],
+                'address' => $data['address'],
+                'coordinates' => $data['coordinates'],
+                'short_description' => $data['short_description'],
+                'start_at' => $data['start_at'],
+                'finish_at' => $data['finish_at'],
+                'author_id' => $data['author_id'],
+                'price' => $data['price'],
+                'active' => '0',
+            ]);
+        }
+    }
      // Метод вывода всех событий
      public function index()
      {
@@ -38,7 +54,7 @@ class EventController extends Controller
          }
      }
 
-    // Метод вывода всех событий в которых текущий пользователь авто
+    // Метод вывода всех событий в которых текущий пользователь автор
      public function eventorganize(){
         if (Auth::check()) {
             $user = Auth::user()->getAuthIdentifier();
