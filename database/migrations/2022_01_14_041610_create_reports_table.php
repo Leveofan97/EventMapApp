045854fa14ddb2ea194de3af8641abf6aa37c1e0
1197 Leveofan <MopsPlayTheme@mail.ru>
2022-01-14 09:30:50 +0500

    Add migration create_reports_table

---
+++ database/migrations/2022_01_14_041610_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('reported_user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('reported_event_id')->references('id')->on('events')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->text("notes")->nullable();
+            $table->integer("state")->nullable();
+            $table->dateTime("created_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
